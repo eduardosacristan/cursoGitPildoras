66fe110de6c895e820198c7141cf48292db51ca2
Eduardo Sacristán Beltri <eduardo.sacristan@inetum.com>
2023-10-03 16:27:41 +0200

    Seguimos metiendo cosas

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello, world");
         System.out.println("Hello again!");
         System.out.println("Otra cosa añadida");
+        System.out.println("Venga , mas cosas para dentro");
     }
 
 }
