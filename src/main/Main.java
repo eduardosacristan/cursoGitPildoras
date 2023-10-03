109cc038208c17e510363607569314dd83eb89b3
Eduardo Sacristán Beltri <eduardo.sacristan@inetum.com>
2023-10-03 16:27:13 +0200

    Agregada otra linea

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main (String[]args){
         System.out.println("Hello, world");
         System.out.println("Hello again!");
+        System.out.println("Otra cosa añadida");
     }
 
 }
