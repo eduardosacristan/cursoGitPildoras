b20f2aac351914443f24710dac32ea1b8219c6a0
Eduardo Sacristán Beltri <eduardo.sacristan@inetum.com>
2023-10-05 11:38:32 +0200

    Hago otro commit local antes del push

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("Acabo de crear otra rama");
         
         System.out.println("Ahopra hago un commit local");
+        System.out.println("Ahopra hago uotro commit local");
     }
 }
 // fin
